6179f231ed3429cfd061bd5a864591f00d967a7b
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-08 09:02:48 -0700

    bash

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("programming is like bulding a multilingal puzzle\n");
+	return (0);
+}
