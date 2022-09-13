40bbd524c417dc977e1026635e4aaf5a5ce77b54
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-13 10:39:52 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print putchar followed by a new line
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	printf("p\nu\nt\nc\nh\na\nr");
+	return (0);
+}
