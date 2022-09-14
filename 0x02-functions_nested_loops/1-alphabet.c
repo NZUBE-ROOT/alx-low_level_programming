bf8a432b9ac1074c15b5c23ef60fafd321a88030
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-13 20:01:15 -0700

    void print_to_98(int n);

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z');
+	{
+	putchar('c');
+		c++;
+	}
+	putchar('\n');
+}
