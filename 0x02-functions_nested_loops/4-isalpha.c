6a23252399aaf29ab1271a356f59bf7262b23849
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-14 08:07:04 -0700

    money

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,8 +3,8 @@
 /**
  *
  * -isalpha - check for alphabetic character
- *  @c: the character to be checked
- *  Return: 1 if cis a letter, 0 otherwise
+ * @c: the character to be checked
+ * Return: 1 if cis a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
