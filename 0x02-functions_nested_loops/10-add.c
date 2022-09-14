63d0edcbea7cfd3908bb67b55a341fd09d9494b3
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-14 00:46:50 -0700

    b165828c06ef2180b200

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - two integer and returs the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * return: sum of a and b
+ */
+void print_to_98(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
