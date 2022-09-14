e109c39ef31381d956090bfe0b73831e71017a15
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-13 21:34:54 -0700

    int _islower(int c);

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isflower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lower, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
