228afe9256d5e175557cc75de5db86db52a16728
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-13 20:35:16 -0700

    but

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_alphabet - Entry point
  * Return: 0
@@ -9,7 +9,8 @@ void print_alphabet(void)
 	char c = 'a';
 
 	while (c <= 'z')
-		_putchar(c);
+	{	_putchar(c);
 		c++;
+	}
 	_putchar('\n');
 }
