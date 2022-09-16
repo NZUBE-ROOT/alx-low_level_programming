80473b1c33a5df238608effef4000426cfbbc3ed
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-15 21:44:14 -0700

    none

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int _isupper(int c)
 {
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
