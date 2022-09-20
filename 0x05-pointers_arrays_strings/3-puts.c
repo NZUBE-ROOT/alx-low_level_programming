c889a5595fad7e46e8746c763e2eab86385d0cee
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-19 18:03:14 -0700

    mum

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	char string[] = "moma";
+		printf("%s", string);
+}
