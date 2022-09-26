786b88e89ae87b35fd2f80f34310f0a6b474cb54
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-26 13:27:41 -0700

    git

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,5 @@
 #define MAIN_H
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-#endif
+unsigned int _strspn(char *s, char *accept);
+#endig
