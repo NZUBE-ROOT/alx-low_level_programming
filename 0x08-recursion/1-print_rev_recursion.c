835c98a5d4c6a72ee2fe6b1e36ba478540ccfcc9
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-09-28 14:09:09 -0700

    my hubby

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+void _print_rev_recursion(char *s)
+	{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
 
