62c1739b8ccd9eb037a7faa427e7443ec80d849d
MomaNzubechukwu <momanzubeolaedo@gmail.com>
2022-10-14 08:58:29 -0700

    mjki

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
+ * print_name - prints a game
  * @name: name to print
  * @f: function to print with
- *
+ * Description: prints a name using function pointers
  * Return: void
+ *
  */
 
 void print_name(char *name, void (*f)(char *))
