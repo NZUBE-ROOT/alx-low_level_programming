1d5f74e241bac189f1b8331640646451bb9288a6
Nzube root <momafransisco@gmail.com>
2024-01-16 01:24:56 +0000

    dynamicli

---
+++ 0x18-dynamic_libraries/mash.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/Lordwill1/alx-low_level_programming/master/0x18-dynamic_libraries/_putchar.c https://raw.githubusercontent.com/Lordwill1/alx-low_level_programming/master/0x18-dynamic_libraries/operations.c
